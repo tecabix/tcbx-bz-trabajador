10aad156e7bf210ed4e4fad47dc9cb4d0ad67917
Angel Abinadi Ramirez Urrutia <abinadi.ramirez.urrutia@tecabix.com>
2025-06-14 16:12:22 -0600

    init

---
+++ src/main/java/com/tecabix/bz/trabajador/Trabajador001BZ.java
@@ -0,0 +1,19 @@
+package com.tecabix.bz.trabajador;
+
+import org.springframework.http.ResponseEntity;
+
+import com.tecabix.res.b.RSB028;
+import com.tecabix.sv.rq.RQSV037;
+
+/**
+ *
+ * @author Ramirez Urrutia Angel Abinadi
+ */
+public class Trabajador001BZ {
+	
+
+	public ResponseEntity<RSB028> crear(RQSV037 rqsv037) {
+		return null;
+	}
+	
+}
---
+++ src/test/java/com/tecabix/bz/trabajador/Trabajador001BZTest.java
@@ -0,0 +1,13 @@
+package com.tecabix.bz.trabajador;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+*
+* @author Ramirez Urrutia Angel Abinadi
+*/
+@ExtendWith(MockitoExtension.class)
+class Trabajador001BZTest {
+	
+}
